242c934bbdbcde0f95ba9d89d09dd105aa74c2e2
Otrebor0417 <roborobert185@gmail.com>
2025-12-16 21:43:57 -0800

    rgrg
    gdgrggr

--- P7FlappyBirdFinalDiaz/Assets/Scripts/Bird.cs
+++ P7FlappyBirdFinalDiaz/Assets/Scripts/Bird.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class Bird : MonoBehaviour
 {
-    public float upForce = 200f;
-    
+    public float upforce = 200f;
+
     private bool isDead = false;
     private Rigidbody2D rb2d;
     private Animator anim;
@@ -25,15 +25,17 @@ public class Bird : MonoBehaviour
             if (Input.GetMouseButtonDown(0))
             {
                 rb2d.velocity = Vector2.zero;
-                rb2d.AddForce (new Vector2(0, upForce));
+                rb2d.AddForce(new Vector2(0, upforce));
                 anim.SetTrigger("Flap");
             }
         }
     }
 
-    void OnCollisionEnter2D ()
+    void OnCollisionEnter2D()
     {
+        rb2d.velocity = Vector2.zero;
         isDead = true;
-        anim.SetTrigger ("Die");
+        anim.SetTrigger("Die");
+        GameControl.instance.BirdDied();
     }
 }
--- P7FlappyBirdFinalDiaz/Assets/Scripts/GameControl.cs
+++ P7FlappyBirdFinalDiaz/Assets/Scripts/GameControl.cs
@@ -1,33 +1,50 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms.Impl;
+using TMPro;
 
 public class GameControl : MonoBehaviour
 {
-    public static GameControl Instance;
+    public static GameControl instance;
     public GameObject gameOverText;
+    public TMP_Text scoreText;
     public bool gameOver = false;
+    public float scrollSpeed = -1.5f;
+    
+    private int score = 0;
     // Start is called before the first frame update
     void Awake()
     {
-        if (Instance == null) { 
-            Instance = this; 
-        } else if (Instance != this)
+        if (instance == null){
+            instance = this;
+        }else if (instance != this)
         {
-            Destroy (gameObject);
+            Destroy(gameObject);
         }
-        
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (gameOver == true && Input.GetMouseButtonDown(0))
+        {
+            SceneManager.LoadScene(SceneManager.GetActiveScene ().buildIndex);
+        }
     }
-
+    public void BirdScored()
+    {
+        if (gameOver)
+        {
+            return;
+        }
+        score++;
+        scoreText.text = "Score: " + score.ToString ();
+    }
     public void BirdDied()
     {
-        gameOverText.SetActive (true);
+        gameOverText.SetActive(true);
         gameOver = true;
-    } 
+    }
 }
